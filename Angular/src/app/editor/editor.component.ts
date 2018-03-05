bc7e101e3d36a96ff10d9b97f3880a5359651c45
Lucas <legon.lucas@gmail.com>
2018-03-05 14:35:08 +0100

    Ajout de la fonctionnalité POUBELLE

--- Angular/src/app/editor/editor.component.ts
+++ Angular/src/app/editor/editor.component.ts
@@ -44,37 +44,65 @@ export class EditorComponent implements OnInit {
         },
 
         applyDropEvents: function(dropper) {
+        
+            if(dropper.className!=="poubelle"){
+                dropper.addEventListener('dragover', function(e) {
+                    e.preventDefault(); // On autorise le drop d'éléments
+                    this.className = 'zone zone_hover'; // Et on applique le style adéquat à notre zone de drop quand un élément la survole
+                });
 
-            dropper.addEventListener('dragover', function(e) {
-                e.preventDefault(); // On autorise le drop d'éléments
-                this.className = 'zone zone_hover'; // Et on applique le style adéquat à notre zone de drop quand un élément la survole
-            });
+                dropper.addEventListener('dragleave', function() {
+                    this.className = 'zone'; // On revient au style de base lorsque l'élément quitte la zone de drop
+                });
 
-            dropper.addEventListener('dragleave', function() {
-                this.className = 'zone'; // On revient au style de base lorsque l'élément quitte la zone de drop
-            });
+                var dndHandler = this; // Cette variable est nécessaire pour que l'événement « drop » ci-dessous accède facilement au namespace « dndHandler »
 
-            var dndHandler = this; // Cette variable est nécessaire pour que l'événement « drop » ci-dessous accède facilement au namespace « dndHandler »
+                dropper.addEventListener('drop', function(e) {
 
-            dropper.addEventListener('drop', function(e) {
+                    var target = e.target,
+                        draggedElement = dndHandler.draggedElement, // Récupération de l'élément concerné
+                        clonedElement = draggedElement.cloneNode(true); // On créé immédiatement le clone de cet élément
+                    while (target.className.indexOf('zone') == -1) { // Cette boucle permet de remonter jusqu'à la zone de drop parente
+                        target = target.parentNode;
+                    }
 
-                var target = e.target,
-                    draggedElement = dndHandler.draggedElement, // Récupération de l'élément concerné
-                    clonedElement = draggedElement.cloneNode(true); // On créé immédiatement le clone de cet élément
+                    target.className = 'zone'; // Application du style par défaut
 
-                while (target.className.indexOf('zone') == -1) { // Cette boucle permet de remonter jusqu'à la zone de drop parente
-                    target = target.parentNode;
-                }
+                    clonedElement = target.appendChild(clonedElement); // Ajout de l'élément cloné à la zone de drop actuelle
+                    dndHandler.applyDragEvents(clonedElement); // Nouvelle application des événements qui ont été perdus lors du cloneNode()
+                    if((draggedElement.parentNode.parentNode.id!=="fonction")&&(draggedElement.parentNode.id!=="teinte")){
+                        draggedElement.parentNode.removeChild(draggedElement); // Suppression de l'élément
+                    }
+                });
+            }
+            
+            else if(dropper.className=="poubelle")
+            {
+                dropper.addEventListener('dragover', function(e) {
+                    e.preventDefault(); // On autorise le drop d'éléments
+                    this.className = 'poubelle poubelle_hover'; // Et on applique le style adéquat à notre zone de drop quand un élément la survole
+                });
 
-                target.className = 'zone'; // Application du style par défaut
+                dropper.addEventListener('dragleave', function() {
+                    this.className = 'poubelle'; // On revient au style de base lorsque l'élément quitte la zone de drop
+                });
 
-                clonedElement = target.appendChild(clonedElement); // Ajout de l'élément cloné à la zone de drop actuelle
-                dndHandler.applyDragEvents(clonedElement); // Nouvelle application des événements qui ont été perdus lors du cloneNode()
-                if((draggedElement.parentNode.parentNode.id!=="fonction")&&(draggedElement.parentNode.id!=="teinte")){
-                    draggedElement.parentNode.removeChild(draggedElement); // Suppression de l'élément
-                }
-            });
+                var dndHandler = this; // Cette variable est nécessaire pour que l'événement « drop » ci-dessous accède facilement au namespace « dndHandler »
 
+                dropper.addEventListener('drop', function(e) {
+
+                    var target = e.target,
+                        draggedElement = dndHandler.draggedElement;
+                    while (target.className.indexOf('poubelle') == -1) { // Cette boucle permet de remonter jusqu'à la zone de drop parente
+                        target = target.parentNode;
+                    }
+
+                    target.className = 'poubelle'; // Application du style par défaut
+                    if((draggedElement.parentNode.parentNode.id!=="fonction")&&(draggedElement.parentNode.id!=="teinte")){
+                        draggedElement.parentNode.removeChild(draggedElement); // Suppression de l'élément
+                    }
+                });
+            }
         }
 
     };
@@ -92,7 +120,9 @@ export class EditorComponent implements OnInit {
     for (var i = 0; i < droppersLen; i++) {
         dndHandler.applyDropEvents(droppers[i]); // Application des événements nécessaires aux zones de drop
     }
-
+    
+    var poubelle = document.querySelector('.poubelle');
+    dndHandler.applyDropEvents(poubelle);
   }
   
   

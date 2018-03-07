69a282a1d4c14013362bbbfc35af3199dbc7ccbe
Lucas <legon.lucas@gmail.com>
2018-03-07 17:33:47 +0100

    Piece degrader fonctionnelle + envoie message

--- Angular/src/app/editor/editor.component.ts
+++ Angular/src/app/editor/editor.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { Piece } from './piece';
+import * as myGlobals from './globals';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
-
 
 
 
@@ -22,7 +22,10 @@ export class EditorComponent implements OnInit {
   constructor(private location: Location) { }
 
   ngOnInit() {
-         /////////////////
+        myGlobals.setIndiceLetter(0);
+        
+        
+        /////////////////
         // DRAG & DROP //
        /////////////////
         
@@ -70,7 +73,7 @@ export class EditorComponent implements OnInit {
 
                     clonedElement = target.appendChild(clonedElement); // Ajout de l'élément cloné à la zone de drop actuelle
                     dndHandler.applyDragEvents(clonedElement); // Nouvelle application des événements qui ont été perdus lors du cloneNode()
-                    if((draggedElement.parentNode.parentNode.id!=="fonction")&&(draggedElement.parentNode.id!=="teinte")){
+                    if((draggedElement.parentNode.parentNode.id!=="piece_fonction")&&(draggedElement.parentNode.id!=="teinte")){
                         draggedElement.parentNode.removeChild(draggedElement); // Suppression de l'élément
                     }
                 });
@@ -131,6 +134,26 @@ export class EditorComponent implements OnInit {
         // Actions aux clics //
        ///////////////////////
 
+// Orientation TOUR
+    
+    goLeft(): void{
+        myGlobals.setIndiceLetter(myGlobals.indice_letter-1);
+        if(myGlobals.indice_letter==-1){
+            myGlobals.setIndiceLetter(3);
+        }
+        var div = document.getElementById('orientation');
+        div.innerHTML=myGlobals.orientation_letter[myGlobals.indice_letter];    
+    }
+    
+    goRight(): void{
+        myGlobals.setIndiceLetter(myGlobals.indice_letter+1);
+        if(myGlobals.indice_letter==4){
+            myGlobals.setIndiceLetter(0);
+        }
+        var div = document.getElementById('orientation');
+        div.innerHTML=myGlobals.orientation_letter[myGlobals.indice_letter];
+    }
+
 // Accueil
     goBack(): void {
         this.location.back();
@@ -144,17 +167,30 @@ export class EditorComponent implements OnInit {
         alert('Travail enregistré');
     }
   
+    goSend(): void {
+        var etage1 = document.getElementById("color_etage1");
+        var etage2 = document.getElementById("color_etage2");
+        var etage3 = document.getElementById("color_etage3");
+        
+        //Protocole :
+        //  X=Etage 1
+        //  Y=Etage 2
+        //  Z=Etage 3
+        
+        var message_etage1 = "X"+sendEtage(etage1,"etage1_down","etage1_middle","etage1_top");
+        var message_etage2 = "Y"+sendEtage(etage2,"etage2_down","etage2_middle","etage2_top");
+        var message_etage3 = "Z"+sendEtage(etage3,"etage3_down","etage3_middle","etage3_top");
+        console.log("Etage 1 : %s",message_etage1);
+        console.log("Etage 2 : %s",message_etage2);
+        console.log("Etage 3 : %s",message_etage3);
+        
+        alert('Message envoyé'); 
+    }
+  
     goUndo(): void {
         alert('Retour arrière');  
     }
- 
- 
- 
- 
- 
- 
- 
- 
+
  
 // Lecture
     start() : void {
@@ -164,17 +200,29 @@ export class EditorComponent implements OnInit {
         var etage2 = document.getElementById("color_etage2");
         var etage3 = document.getElementById("color_etage3");
         
-        
         playEtage(etage1,"etage1_down","etage1_middle","etage1_top");
         playEtage(etage2,"etage2_down","etage2_middle","etage2_top");
         playEtage(etage3,"etage3_down","etage3_middle","etage3_top");
     }
 
 
+
+
+
+
+
+
     
     stop() : void {
     
     }
+
+
+
+
+
+
+
 
 
 // Couleur
@@ -235,8 +283,66 @@ export class EditorComponent implements OnInit {
        ///////////////////////////////
 
 
+
+function sendEtage(etage,down,middle,top){
+
+    // Initialisation de l'éclairage //
+    etage.style.backgroundColor = "silver";
+    
+    // TABLEAU DE COULEURS TOP //
+    var tab_color_top = new Array();
+    tab_color_top = tableauCouleur(top);
+    var tab_color_top_len = tab_color_top.length;
+    
+    // TABLEAU DE COULEURS DOWN //
+    var tab_color_down = new Array();
+    tab_color_down = tableauCouleur(down);
+    var tab_color_down_len = tab_color_down.length;
+    
+    // TABLEAU DES IDs DES FONCTIONS //
+    var tab_id = new Array();
+    tab_id = tableauId(middle);
+    var tab_id_len = tab_id.length;
+    
+    // TABLEAU DES PIECES FONCTIONS //
+    var tab = new Array();
+    tab = tableauPiece(tab_id,tab_id_len,tab_color_top,tab_color_top_len,tab_color_down,tab_color_down_len);
+    if(tab==null){
+        alert('Il y a une erreur dans ta programmation');
+    }
+    else{
+        // CREATION DE LA CHAINE //
+        var tempo = getTempo()*1000;    // Récupération de la durée des pièces
+        var tab_len = tab.length;
+        var chaine = creerChaineProtocole(tab,tab_len,tempo);
+        return chaine;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Fonction principale faisant le traitement pour un étage passé en paramètre
 function playEtage(etage,down,middle,top){
+
+    // Initialisation de l'éclairage //
+    etage.style.backgroundColor = "silver";
+    
     // TABLEAU DE COULEURS TOP //
     var tab_color_top = new Array();
     tab_color_top = tableauCouleur(top);
@@ -260,9 +366,10 @@ function playEtage(etage,down,middle,top){
     }
     else{
         // ALLUMAGE DE LA TOUR //
+        var tempo = getTempo()*1000;    // Récupération de la durée des pièces
         var tab_len = tab.length;
         var i = 0;
-        playTour(etage,tab,tab_len,i,1000);
+        playTour(etage,tab,tab_len,i,tempo);
     }
 }
 
@@ -318,23 +425,25 @@ function tableauPiece(tab_id, tab_id_len, tab_color_top, tab_color_top_len,tab_c
     {
         var tab_eta = new Array(tab_id_len);
         var k=0;
+        var l=0;
         for (var i = 0; i < tab_id_len; i++) {
             if(tab_id[i]==="Allumer"){
-                tab_eta[i]=new Piece(tab_color_top[k],"Allumer");
+                tab_eta[i]=new Piece(tab_color_top[k],"null","Allumer");
                 k++;
             }
             
             else if(tab_id[i]==="Eteindre"){
-                tab_eta[i]=new Piece("silver","Eteindre");
+                tab_eta[i]=new Piece("silver","null","Eteindre");
             }
             
             else if(tab_id[i]==="Degrader"){
-                tab_eta[i]=new Piece(tab_color_top[k],"Degrader");
+                tab_eta[i]=new Piece(tab_color_top[k],tab_color_down[l],"Degrader");
                 k++;
+                l++;
             }
             
             else if(tab_id[i]==="Clignoter"){
-                tab_eta[i]=new Piece(tab_color_top[k],"Clignoter");
+                tab_eta[i]=new Piece(tab_color_top[k],"null","Clignoter");
                 k++;
             }
         }
@@ -360,7 +469,7 @@ function playTour(etage,tab_etage,tab_etage_len,i,tempo) {
         }
 
         else if(tab_etage[i].getId()==="Degrader"){
-            degraderTour(etage,tab_etage[i]);            
+            degraderTour(etage,tab_etage[i],120,6*tempo/1000);            
         }
 
         else if(tab_etage[i].getId()==="Clignoter"){
@@ -383,9 +492,40 @@ function allumerTour(etage,piece){
 
 
 // Fonction appelé lorsque nous avons une piece DEGRADER
-function degraderTour(etage,piece){
+function degraderTour(etage,piece,nbTrans,delay){
     console.log("%s",piece.getId());
-    etage.className='classname';
+    var styleTop = piece.getStyle().slice(4,-1).split(','); // Récupérer les couleurs rouge, vert, bleu dans 3 variables différentes
+    var styleDown = piece.getStyleDegra().slice(4,-1).split(',');
+
+    var tab_top = [parseInt(styleTop[0],10),parseInt(styleTop[1],10),parseInt(styleTop[2],10)];
+    var tab_down = [parseInt(styleDown[0],10),parseInt(styleDown[1],10),parseInt(styleDown[2],10)];
+    
+    var delay = delay || 100; // en millisecondes c ets le temps qui passe entre chaque execution. a diminuer si la fluidité n est pas au rendez vous
+    var nbTrans = nbTrans || 20; // le nombre de transitions...a augmenter si ce n ets pas assez fluide
+    var startColor = tab_top || [255,255,255]; // remplacer par les chiffres qui vont bien
+    var stopColor = tab_down || [255,255,255]; // pareil
+    
+    // maintenant il faut calculer le delta de chaque couleur
+    var rDelta = Math.floor((stopColor[0]-startColor[0])/nbTrans);
+    var gDelta = Math.floor((stopColor[1]-startColor[1])/nbTrans);
+    var bDelta = Math.floor((stopColor[2]-startColor[2])/nbTrans);
+    
+     // on cree les variables ou vont etre stockees les couleurs temporaires
+    var rTemp = startColor[0];
+    var gTemp = startColor[1];
+    var bTemp = startColor[2];
+ 
+    // un petit setInterval pour que la fonction se repete d elle meme
+    var interval = setInterval(function() {
+        rTemp += rDelta;
+        gTemp += gDelta;
+        bTemp += bDelta;
+        etage.style.backgroundColor = "rgb("+rTemp+","+gTemp+","+bTemp+" )";
+        if (rTemp<0 || gTemp<0 || bTemp<0|| rTemp>255 || gTemp>255 || bTemp>255 ) {
+            clearInterval(interval);
+            etage.style.backgroundColor = "rgb("+stopColor[0]+","+stopColor[1]+","+stopColor[2]+" )";
+        }
+    },delay);
 }
 
 
@@ -409,4 +549,62 @@ function clignoterTour(etage,piece,nb_cligno,tempo){
         nb_cligno--;
         setTimeout(clignoterTour, tempo,etage,piece,nb_cligno,tempo);
     }
+}
+
+// Fonction qui nous renvoie la durée que dois avoir les pièces, en secondes
+function getTempo(){
+    return (parseFloat((<HTMLInputElement>document.getElementById("duree_input")).value));
+}
+
+
+
+
+
+
+
+
+function creerChaineProtocole(tab_etage,tab_etage_len,tempo){
+    //Protocole :
+    //  T=Temporisation
+    //  L=Allumer
+    //  O=Eteindre
+    //  R=Degrader
+    //  P=Clignoter
+    
+    var chaine;
+    if(tab_etage_len===0)
+    {
+        chaine = "null";
+    }
+    
+    else {
+        chaine = "T"+tempo;
+        for (var i = 0; i < tab_etage_len; i++){
+            if(tab_etage[i].getId()==="Allumer"){
+                chaine = chaine+"L"+rgbToHex(tab_etage[i].getStyle());
+            }
+
+            else if(tab_etage[i].getId()==="Eteindre"){
+                chaine = chaine+"O"+rgbToHex(tab_etage[i].getStyle());
+            }
+
+            else if(tab_etage[i].getId()==="Degrader"){
+                chaine = chaine+"R"+rgbToHex(tab_etage[i].getStyle())+rgbToHex(tab_etage[i].getStyleDegra());           
+            }
+
+            else if(tab_etage[i].getId()==="Clignoter"){
+                chaine = chaine+"P"+rgbToHex(tab_etage[i].getStyle()); 
+            }
+        }
+    }
+    
+    return chaine;
+}
+
+
+function rgbToHex(style) {
+    var stylergb = style.slice(4,-1).split(',');
+
+    var tab_rgb = [parseInt(stylergb[0],10),parseInt(stylergb[1],10),parseInt(stylergb[2],10)];
+    return ((1 << 24) + (tab_rgb[0] << 16) + (tab_rgb[1] << 8) + tab_rgb[2]).toString(16).slice(1);
 }
---
+++ Angular/src/app/editor/globals.ts
@@ -0,0 +1,16 @@
+// Orientation de la tour //
+
+export var orientation_letter = ['N','E','S','W'];
+export var indice_letter;
+export function setIndiceLetter(newValue: number) {
+    indice_letter = newValue;
+}
+
+
+
+// Durée des pièces //
+
+export var tempo;
+export function setTempo(newValue: number) {
+    tempo = newValue;
+}
